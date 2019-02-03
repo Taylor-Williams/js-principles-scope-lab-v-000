4f6fb0c0f199ba3bbe55fc1d45d56cc9458b83d1
Taylor Williams <taylor@thewilliams.net>
2019-02-03 08:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function upperCaseCustomerName(){
   customerName = customerName.toUpperCase();
 }
 function setBestCustomer(){
-  return bestCustomer = 'not bob'
+  return bestCustomer = 'not bob';
 }
